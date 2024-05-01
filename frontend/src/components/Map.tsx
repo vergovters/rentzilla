fe8453a9932cc2174d3c693723e5dd18b5e4ce76
illia <fialkivskiji@gmail.com>
2024-05-01 20:32:02 +0300

    clusters and markers

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,15 +1,52 @@
 import "./Map.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { Icon, divIcon, point } from "leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
+
+const markers = [
+  {
+    geocode: [50.444, 30.521],
+    popUp: "Hello, I am pop up 1"
+  },
+  {
+    geocode: [50.443, 30.522],
+    popUp: "Hello, I am pop up 2"
+  },
+  {
+    geocode: [50.441, 30.52],
+    popUp: "Hello, I am pop up 3"
+  }
+];
 
 const Mambox = () => {
+  const position = [50.44, 30.52];
+
+  const createClusterCustomIcon = function (cluster: any) {
+    return new divIcon({
+      html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
+      className: "custom-marker-cluster",
+      iconSize: point(33, 33, true)
+    });
+  };
 
   return (
-    <MapContainer center={[48.8566, 2.3522]} zoom={13}>
+    <MapContainer center={position} zoom={13}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+        <MarkerClusterGroup
+          chunkedLoading
+          iconCreateFunction={createClusterCustomIcon}
+        >
+          {markers.map((marker) => (
+            <Marker position={marker.geocode} >
+              <Popup>{marker.popUp}</Popup>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
+        
     </MapContainer>
   );
 }
